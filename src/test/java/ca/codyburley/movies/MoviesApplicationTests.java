799eb91ed88ede323eef6f95df2f0cca81f58071
Cody Burley <codyburley@gmail.com>
2023-03-14 12:54:28 -0400

    first commit

---
+++ src/test/java/ca/codyburley/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.codyburley.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
